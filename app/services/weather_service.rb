ede619813078fb942bf469973d56c6b6c4c1c524
Freddy Monroy <fredydeltoro@gmail.com>
2023-11-24 00:14:55 -0600

    Refactoring weather service as a module

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,14 +1,14 @@
-class WeatherService
-  def initialize()
-    @connection = Faraday.new('https://api.openweathermap.org/data/2.5/onecall') do |conn|
+module WeatherService
+  BASE_URL = 'https://api.openweathermap.org/data/2.5/onecall'.freeze
+
+  def self.get_temperature_by_cordinates(lat, lon)
+    connection = Faraday.new(BASE_URL) do |conn|
       conn.params['appid'] = OWMApiKey
       conn.params['units'] = 'metric'
       conn.adapter Faraday.default_adapter
     end
-  end
 
-  def get_temperature_by_cordinates(lat, lon)
-    response = @connection.get do |req|
+    response = connection.get do |req|
       req.params['lat'] = lat
       req.params['lon'] = lon
       req.params['exclude'] = 'current,minutely,hourly'
