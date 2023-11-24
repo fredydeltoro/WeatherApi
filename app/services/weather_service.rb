cf286413a266b83007b77e88b220082324d05d67
Freddy Monroy <fredydeltoro@gmail.com>
2023-11-24 13:16:09 -0600

    Return only min and max temp with weather service

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,21 +1,35 @@
 module WeatherService
   BASE_URL = 'https://api.openweathermap.org/data/2.5/onecall'.freeze
+  CONNECTION = Faraday.new(BASE_URL) do |conn|
+    conn.params['appid'] = OWMApiKey
+    conn.params['units'] = 'metric'
+    conn.params['exclude'] = 'current,minutely,hourly,alerts'
+    conn.adapter Faraday.default_adapter
+  end
 
   def self.get_temperature_by_cordinates(lat, lon)
-    connection = Faraday.new(BASE_URL) do |conn|
-      conn.params['appid'] = OWMApiKey
-      conn.params['units'] = 'metric'
-      conn.adapter Faraday.default_adapter
-    end
-
-    response = connection.get do |req|
+    response = CONNECTION.get do |req|
       req.params['lat'] = lat
       req.params['lon'] = lon
-      req.params['exclude'] = 'current,minutely,hourly'
     end
 
     returns nil unless response.success?
 
-    JSON.parse(response.body)
+    body = JSON.parse(response.body)
+
+    # Maping daily to get only the min and max temp
+    daily = body["daily"].map do |day|
+      # Convert epoch to date
+      date = DateTime.strptime(day["dt"].to_s,'%s')
+      date = date.to_date
+      date = date.strftime
+
+      {
+        date: date,
+        temp: { min: day["temp"]["min"], max: day["temp"]["max"] }
+      }
+    end
+
+    daily
   end
 end
